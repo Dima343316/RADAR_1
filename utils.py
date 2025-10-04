9b81a3de3ec94030af354fc9a713e74c7390c5bc
Dima343316 <72628630+Dima343316@users.noreply.github.com>
2025-10-04 21:52:19 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 API_KEY = os.getenv("VSE_GPT_API_KEY")
 
 class RadarAnalyzer:
